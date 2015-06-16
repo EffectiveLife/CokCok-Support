4a93191eb936a6c36bfca791b40253269340f053
JayD <jaedory80@naver.com>
2015-06-16 18:08:47 +0900

    Volley Extensions 중 Volley-caches 적용.
     1. VolleyImprovedCache : RequestQueueManager에서 사용하는 기본 구현체 Volley 대신 RequestQueue 생성하는 커스텀 클래스 추가
      - 해당 클래스는 기존 Volley 구현체와 완전 동일하지만 기본 DiskBasedCache가 아닌 Volley-caches의 DiskBasedCache 클래스 사용
     2. ReqQueueManager에서 구현체 호출 부분 변경
     3. module build.gradle 파일에서 android defaultConfig 중 versionName 전역변수 libraryVersion 대입

--- cokcok-support/src/main/java/com/effectivelife/cokcoksupport/net/ReqQueueManager.java
+++ cokcok-support/src/main/java/com/effectivelife/cokcoksupport/net/ReqQueueManager.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.Volley;
 import com.effectivelife.cokcoksupport.custom.BitmapLruCache;
+import com.effectivelife.cokcoksupport.custom.VolleyImprovedCache;
 
 /**
  * Created by com on 2015-06-03.
@@ -21,7 +21,7 @@ public class ReqQueueManager {
     private ImageLoader mImageLoader;
 
     private ReqQueueManager(Context context) {
-        mContext = context;
+        mContext = context.getApplicationContext();
         mRequestQueue = getReqQueue();
         mImageLoader = new ImageLoader(mRequestQueue, new BitmapLruCache());
     }
@@ -35,7 +35,7 @@ public class ReqQueueManager {
 
     public RequestQueue getReqQueue() {
         if(mRequestQueue == null) {
-            mRequestQueue = Volley.newRequestQueue(mContext);
+            mRequestQueue = VolleyImprovedCache.newRequestQueue(mContext);
         }
         return mRequestQueue;
     }
